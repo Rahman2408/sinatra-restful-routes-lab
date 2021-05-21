dca1f596222584d9bffdba04e6e8f488150a0dac
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-05-21 09:23:22 -0400

    commit tree b60d2045e86c30ce201d0dcccc16d36b73ce98e7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,50 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+    get '/recipes' do 
+      erb :index
+    end
 
+   
+
+    get '/recipes/new' do 
+      erb :new 
+    end
+   
+    
+    post '/recipes/new' do 
+      @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+      @recipe.save
+      redirect "/recipes/#{@recipe.id}"
+    end
+
+    
+    get '/recipes/:id' do 
+      @recipe= Recipe.find(params[:id])
+      
+      erb :show
+    end
+
+    post '/recipes' do 
+      @recipe = Recipe.create(params)
+      redirect "/recipes/#{@recipe.id}"
+    end
+
+    delete '/recipes/:id' do 
+      @recipe = Recipe.find(params[:id])
+      @recipe.delete
+      redirect '/recipes'
+    end
+
+    get '/recipes/:id/edit' do
+      @recipe = Recipe.find(params[:id])
+      erb :edit 
+    end
+
+    patch '/recipes/:id/edit' do
+      @recipe = Recipe.find(params[:id])
+ 
+      @recipe.update(name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients])
+      redirect "/recipes/#{@recipe.id}"
+    end
 end
